9fa3c9947d113d0f7cddabd50317478d201419b3
sadam <sadam@sadams-iMac.local>
2024-11-23 17:45:50 +0500

    signup api done

--- backend/index.js
+++ backend/index.js
@@ -29,7 +29,7 @@ app.use("/api/auth", authRoutes);
 
 
 
-app.listen(PORT, "0.0.0.0", () => {
+app.listen(PORT, () => {
   databaseConnection(); // database connection
   console.log(`Server is running on http://localhost:${PORT}`);
 });
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,9 +1,45 @@
 import express from "express";
+import { User } from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
+
+const SECRET = process.env.SECRET;
 
 const router = express.Router();
 
-router.post("/signup", (req, res) => {
-  res.send("hello from sign up");
+router.post("/signup", async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    if (!name || !email || !password) {
+      throw new Error("All fields are required");
+    }
+    const userAlreadyExists = await User.findOne({ email });
+    if (userAlreadyExists) {
+      return res
+        .status(400)
+        .json({ success: false, message: "User already exists" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new User({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    await user.save();
+
+    // token ---- jwt ----
+    const token = jwt.sign({ name, email }, SECRET, { expiresIn: "1h" });
+
+    res
+      .status(201)
+      .json({ success: true, message: "User created successfully" });
+  } catch (error) {
+    res.status(400).json({ success: false, message: error.message });
+  }
 });
 
 router.get("/login", (req, res) => {
