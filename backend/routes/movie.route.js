7e8b1b3310c12b5f4634bf25d05da4a2a5f162bc
sadam-pk <sadamccr@gmail.com>
2024-11-28 03:59:01 +0500

    updated

--- backend/routes/movie.route.js
+++ backend/routes/movie.route.js
@@ -17,7 +17,4 @@ router.post("/favorites", authenticateJwt, addFavorites);
 // get all the user's favorite movies
 router.get("/favorites", authenticateJwt, getFavorites);
 
-// remove a movie from the user's favorites
-router.delete("/favorites/:movieId", authenticateJwt, removeFavorite);
-
 export default router;
