05937db18846b02d157df73b2dbae0853994fbc0
sadam-pk <sadamccr@gmail.com>
2024-11-28 14:37:40 +0500

    deployment update

--- backend/index.js
+++ backend/index.js
@@ -7,19 +7,34 @@ import authRoutes from "./routes/auth.route.js";
 import moviesRoutes from "./routes/movie.route.js";
 import { databaseConnection } from "./db/dbConnection.js";
 
-const PORT = process.env.PORT || 3000;
+const prodOrigins = [process.env.ORIGIN_1, process.env.ORIGIN_2];
+const devOrigin = ["http://localhost:5173"];
 
+const allowedOrigins =
+  process.env.NODE_ENV === "production" ? prodOrigins : devOrigin;
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (allowedOrigins.includes(origin)) {
+        console.log(origin, allowedOrigins);
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+
+    // origin: "http://localhost:5173", // frontend domain
+    credentials: true, // for cookies
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
+
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
 app.use(cookiesParser());
-
-app.use(
-  cors({
-    origin: "http://localhost:5173", // frontend domain
-    credentials: true, // for cookies
-  })
-);
 app.use("/api/auth", authRoutes);
 app.use("/api/movies", moviesRoutes);
 
