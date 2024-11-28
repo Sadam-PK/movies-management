1695d580bebc9d46a5b98af37255537e61bbe08d
sadam-pk <sadamccr@gmail.com>
2024-11-28 15:31:26 +0500

    deployment fixes

--- backend/index.js
+++ backend/index.js
@@ -7,31 +7,15 @@ import authRoutes from "./routes/auth.route.js";
 import moviesRoutes from "./routes/movie.route.js";
 import { databaseConnection } from "./db/dbConnection.js";
 
-const prodOrigins = [process.env.ORIGIN_1, process.env.ORIGIN_2];
-const devOrigin = ["http://localhost:5173"];
-
-const allowedOrigins =
-  process.env.NODE_ENV === "production" ? prodOrigins : devOrigin;
+const PORT = process.env.PORT || 3000;
+const app = express();
 
 app.use(
   cors({
-    origin: (origin, callback) => {
-      if (allowedOrigins.includes(origin)) {
-        console.log(origin, allowedOrigins);
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-
-    // origin: "http://localhost:5173", // frontend domain
+    origin: "http://localhost:5173", // frontend domain
     credentials: true, // for cookies
-    methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
-
-const PORT = process.env.PORT || 3000;
-const app = express();
 
 app.use(express.json());
 app.use(cookiesParser());
