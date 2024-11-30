e2ab792ba4af2950851ce8a875a8c6d391583567
sadam-pk <sadamccr@gmail.com>
2024-12-01 01:52:32 +0500

    updated indexjs

--- backend/index.js
+++ backend/index.js
@@ -17,26 +17,26 @@ const prodOrigins = [process.env.ORIGIN_2];
 const allowedOrigins =
   process.env.NODE_ENV === "production" ? prodOrigins : devOrigin;
 
-  app.use(
-    cors({
-      origin: (origin, callback) => {
-        if (!origin || allowedOrigins.includes(origin)) {
-          callback(null, true);
-        } else {
-          callback(new Error("Not allowed by CORS"));
-        }
-      },
-      methods: ["GET", "POST", "PUT", "DELETE"],
-      allowedHeaders: ["Content-Type", "Authorization"], // Ensure Authorization is allowed
-      credentials: true, // Allow credentials (cookies, authorization headers)
-    })
-  );
-  
-  
-  app.options("*", cors()); // Handle preflight requests for all routes
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"], // Ensure Authorization header is allowed
+    credentials: true, // Allow credentials (cookies, authorization headers)
+  })
+);
+
+// Preflight request handling (for all routes)
+app.options("*", cors()); // This ensures OPTIONS preflight requests are properly handled
 
 app.use(express.json());
-app.use(cookiesParser()); // Not needed if you're not using cookies
+app.use(cookiesParser());
 
 // Use routes
 app.use("/api/auth", authRoutes);
